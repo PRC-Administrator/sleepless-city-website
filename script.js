9f5f73a9e7de3e22092662f1a2cc1e893104e3a1
DEV || Justin <justinzudolski2011@gmail.com>
2026-03-23 14:42:46 +0100

    Upload
    .

---
+++ script.js
@@ -0,0 +1,12 @@
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+    cards.forEach(card => {
+        const pos = card.getBoundingClientRect().top;
+
+        if (pos < window.innerHeight - 50) {
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
